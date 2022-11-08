1b6ab25e020f9d2252498c77c38c6132b4d7051f
Marjan Karafiloski <karafiloski84@gmail.com>
2022-11-08 16:43:55 -0500

    up to 83 x 2

--- portal/portalComponents/PortalValidations.js
+++ portal/portalComponents/PortalValidations.js
@@ -1,4 +1,4 @@
-    const BasePage = require('../BasePage')
+    const BasePage = require('../../BasePage')
     const assert = require('assert')
     const INPUT_VALIDATIONS = { xpath: "//div[contains(@class , 'invalid-feedback')]" }
 
--- test/PortalAndEmbedTests.js
+++ test/PortalAndEmbedTests.js
@@ -81,7 +81,7 @@
         let receipt;
         let steps;
 
-        let base = 586607 // Math.floor(100000 + Math.random() * 900000);
+        let base =  Math.floor(100000 + Math.random() * 900000);
         let eventName =  base.toString() + " FullEventName";
         let shortName = base.toString();
         let ticketOneName = base.toString() +"T1";
