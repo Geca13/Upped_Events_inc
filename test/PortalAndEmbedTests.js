7449e24d7a6a5e47e3eba2822cefb017fa853720
Marjan Karafiloski <karafiloski84@gmail.com>
2022-12-02 17:01:35 -0500

    Go again in cloud 2

--- test/PortalAndEmbedTests.js
+++ test/PortalAndEmbedTests.js
@@ -126,6 +126,7 @@
         beforeEach(async function(){
             driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().addArguments('--headless')).build();
             await driver.manage().window().setRect({width: 1300, height: 1080});
+
         });
 
         afterEach(async function(){
